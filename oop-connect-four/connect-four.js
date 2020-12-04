8d7f4b61db3df8e1a546eb817a378a4bc14c92a3
Nick <nkuek1@gmail.com>
2020-12-04 18:44:09 -0500

    trying to add is-invisible

--- oop-connect-four/connect-four.js
+++ oop-connect-four/connect-four.js
@@ -1,22 +1,53 @@
-import { Game } from `./game.js`
-
+import { Game } from './game.js'
 let game = undefined;
+
+function updateUI() {
+    const boardHolder = document.getElementById('board-holder')
+    const gameName = document.getElementById('game-name');
+    if (game === undefined) {
+        boardHolder
+        .classList
+        .add('is-invisible');
+    } else {
+        boardHolder
+            .classList
+            .remove('is-invisible');
+        gameName.innerHTML = game.getName()
+    }
+
+
+
+}
 
 window.addEventListener("DOMContentLoaded", () => {
 
+
     const player1 = document.getElementById('player-1-name')
     const player2 = document.getElementById('player-2-name')
-    const formHolder = document.getElementById('form-holder')
+    const newGameBtn = document.getElementById('new-game')
+
+
+
+    function enableBtn () {
+        const name1Input = player1.value
+        const name2Input = player2.value
+        newGameBtn.disabled = name1Input.length === 0 ||  name2Input.length === 0;
+    };
 
     player1.addEventListener('keyup', () => {
-        const newGameBtn = document.getElementById('new-game')
-        newGameBtn.disabled = player1.value === '' ||  player2.value === ''
-    })
+        enableBtn();
+    });
 
     player2.addEventListener('keyup', () => {
-        const newGameBtn = document.getElementById('new-game')
-        newGameBtn.disabled = player1.value === '' ||  player2.value === ''
+        enableBtn();
+    });
+
+    newGameBtn.addEventListener('click', () => {
+        game = new Game(player1.value, player2.value);
+        newGameBtn.disabled = true;
+        updateUI();
     })
+
 
 
 })
--- oop-connect-four/game.js
+++ oop-connect-four/game.js
@@ -1,4 +1,4 @@
-export default class Game {
+export class Game {
     constructor(player1, player2){
         this.player1 = player1
         this.player2 = player2
